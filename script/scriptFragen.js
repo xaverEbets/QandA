fe6e1e092488a9601fd102259f8bb65f4215966d
XaverEbets <xaver.ebetshuber@gmail.com>
2024-03-07 20:05:05 +0100

    intensive Changes

--- script/scriptFragen.js
+++ script/scriptFragen.js
@@ -16,3 +16,18 @@ const answerArr = [
   ['Gelb'],
   ['Grün']
 ]
+
+let output = document.getElementById('fragenOutput')
+
+for (i = 0; i < questionArr.length; i++) {
+  const newDiv = document.createElement('div')
+  let question = document.createTextNode(questionArr[i])
+  let answer = document.createTextNode(answerArr[i][0])
+  let br = document.createElement('br')
+  newDiv.appendChild(question)
+  newDiv.appendChild(br)
+  newDiv.appendChild(answer)
+
+  output.appendChild(newDiv)
+  newDiv.classList.add('outputDiv')
+}
